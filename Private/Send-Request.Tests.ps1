0ad545b556caa5b80cc3b001a9a16c35c77dadef
Craig Buchanan <craig.buchanan@cogniza.com>
2020-02-20 14:29:01 -0600

    initial implementation

---
+++ Private/Send-Request.Tests.ps1
@@ -0,0 +1,168 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# dot-source dependencies
+$Parent = Split-Path -Parent $here
+. "$Parent/Private/ConvertTo-PlainText.ps1"
+
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Send-Request" {
+    
+    # arrange
+    $sender_id = 'sender_id'
+    $sender_password = 'sender_password'
+
+    $SecureString = ConvertTo-SecureString $sender_password -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential($sender_id, $SecureString)
+
+    $Login = [PSCustomObject]@{userid='user_id';companyid='Acme';password='user_password'}
+    $Session = [PSCustomObject]@{sessionid='01324656789';endpoint='https://xxx.yyy.zzz/'}
+    $Function = "<function controlid='$( New-Guid )'></function>"
+    
+    Context "ForLogin parameter set" {
+        
+        # arrange
+        $Function = "<function controlid='$( New-Guid )'><getAPISession /></function>"
+
+        Mock Invoke-WebRequest
+
+        # act
+
+        # Get-Command "Send-Request" | Should -HaveParameter Credential -Mandatory
+        # Get-Command "Send-Request" | Should -HaveParameter Login -Mandatory
+        # Get-Command "Send-Request" | Should -HaveParameter Function -Mandatory
+        # Get-Command "Send-Request" | Should -Not -HaveParameter Session
+
+        BeforeEach {
+            Send-Request -Credential $Credential -Login $Login -Function $Function
+        }
+        
+        it "uses the correct method, content-type, and Uri" {
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Method -eq 'Post' }
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $ContentType -eq 'application/xml' }
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -eq 'https://api.intacct.com/ia/xml/xmlgw.phtml' }
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { ([xml]$Body) }
+        }    
+
+        it "-Credential creates the correct request/control elements" {
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                $xml = ([xml]$Body).request.control
+                $xml.senderid -eq $sender_id -and
+                $xml.password -eq $sender_password
+                $xml.uniqueid -eq 'false'
+            }
+        }
+
+        it "-Login creates the correct request/operation/authentication/login elements" {
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                $xml = ([xml]$Body).request.operation.authentication.login
+                $xml.companyid -eq $Login.companyid -and
+                $xml.userid -eq $Login.userid -and
+                $xml.password -eq $Login.password
+            }
+        }
+     
+        it "-Function creates the correct requestion/operation/content/function element" {
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                $xml = ([xml]$Body).request.operation.content
+                $xml.function -ne $null
+                $xml.function.getAPISession -ne $null
+            }
+        }
+
+    }
+
+    Context "ForSession parameter set" {
+
+        Mock Invoke-WebRequest
+
+        # act
+        # Get-Command "Send-Request" | Should -HaveParameter Credential -Mandatory
+        # Get-Command "Send-Request" | Should -HaveParameter Session -Mandatory
+        # Get-Command "Send-Request" | Should -HaveParameter Function -Mandatory
+        # Get-Command "Send-Request" | Shoul -HaveParameter Unique
+        # Get-Command "Send-Request" | Should -HaveParameter Transaction
+        # Get-Command "Send-Request" | Should -Not -HaveParameter Login
+
+        BeforeEach {
+            Send-Request -Credential $Credential -Session $Session -Function $Function -Unique -Transaction
+        }
+
+        it "-Session creates the correct request/control/uniqueid element" {
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -eq $Session.endpoint }
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                $xml = ([xml]$Body).request.operation.authentication
+                $xml.sessionid -eq $Session.sessionid
+            }
+        }
+
+        it "-Function creates the correct requestion/operation/content/function element" {
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                $xml = ([xml]$Body).request.operation.content
+                $xml.function -ne $null
+            }
+        }
+
+        it "-Unique creates the correct request/control/uniqueid element" {
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                $xml = ([xml]$Body).request.control
+                $xml.uniqueid -eq 'true'
+            }
+        }
+
+        it "-Transaction creates the correct request/control/transaction element" {
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter { 
+                $xml = ([xml]$Body).request.operation
+                $xml.GetAttribute('transaction') -eq 'true'
+            }
+        }
+
+    }
+
+    Context "Response" {
+
+        # arrange
+        $Function = "<function controlid='$( New-Guid )'></function>"
+
+        Mock Invoke-WebRequest { 
+            $Response = New-MockObject -Type  Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject
+            $Content =
+                '<?xml version="1.0" encoding="UTF-8"?>
+                <response>
+                    <control><status>success</status></control>
+                    <operation><result><status>success</status></result></operation>
+                </response>'
+            $Response | Add-Member -Type NoteProperty -Name 'Content' -Value $Content -Force
+            $Response
+        }
+
+        it "returns the response's Content as Xml" {
+            $Actual = Send-Request -Credential $Credential -Session $Session -Function $Function
+            $Actual | Should -BeOfType XML
+        }
+    }
+
+    Context "Exception handling" {
+
+        Mock Invoke-WebRequest { 
+            $Response = New-Object System.Net.Http.HttpResponseMessage 401
+            $Phrase = 'Response status code does not indicate success: 401 (Unauthorized).'
+            Throw New-Object Microsoft.PowerShell.Commands.HttpResponseException $Phrase, $Response
+        }
+
+        It "generates an unauthorized (401)" {
+            { Send-Request -Credential $Credential -Session $Session -Function $Function -ErrorAction Stop } | Should -Throw 'Response status code does not indicate success: 401 (Unauthorized).'
+        }
+
+    }
+
+}
---
+++ Private/Send-Request.ps1
@@ -0,0 +1,112 @@
+<#
+.SYNOPSIS
+Sends an XML request to Intacct's API endpoint.
+
+.PARAMETER Credential
+PsCredential object that contains the sender_id and sender_password
+
+.PARAMETER Session
+PsCustomObject that contains the session_id and endpoint.
+
+.PARAMETER Login
+PsCustomObject that contains the user_id, company_id, and user_password.
+
+.PARAMETER Function
+One or more function nodes that contain the verbs and nouns to be processed.
+
+.PARAMETER Unique
+Sets the control's uniqueid element to $true.
+
+.PARAMETER Transaction
+Sets the operation's transaction attribute to $true.
+
+#>
+function Send-Request {
+
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(ParameterSetName='ForLogin', Mandatory)]
+        [Parameter(ParameterSetName='ForSession', Mandatory)]
+        [pscredential]$Credential,
+
+        [Parameter(ParameterSetName='ForSession', Mandatory)]
+        [pscustomobject]$Session,
+
+        [Parameter(ParameterSetName='ForLogin', Mandatory)]
+        [pscustomobject]$Login,
+
+        [Parameter(ParameterSetName='ForLogin', Mandatory)]
+        [Parameter(ParameterSetName='ForSession', Mandatory)]
+        [Parameter( Mandatory)]
+        [string]$Function,
+
+        [Parameter(ParameterSetName='ForSession')]
+        [switch]$Unique,
+
+        [Parameter(ParameterSetName='ForSession')]
+        [switch]$Transaction
+    )
+
+    Write-Debug "$($MyInvocation.MyCommand.Name)"
+    
+    $Uri = 
+        if ($Session.endpoint) {$Session.endpoint} 
+        else {'https://api.intacct.com/ia/xml/xmlgw.phtml'}
+    Write-Debug "Uri: $Uri"
+
+    $Authentication = 
+        if ($Login)
+        {
+            "<login>
+                <userid>$( $Login.userid )</userid>
+                <companyid>$( $Login.companyid )</companyid>
+                <password>$( $Login.password )</password>
+            </login>"
+        }
+        elseif ($Session)
+        {
+            "<sessionid>$($Session.sessionid)</sessionid>"
+        }
+
+    $Body=
+@"
+<?xml version="1.0" encoding="UTF-8"?>
+<request>
+    <control>
+        <senderid>$( $Credential.UserName )</senderid>
+        <password>$( $Credential | ConvertTo-PlainText )</password>
+        <controlid>$( Get-Date -UFormat %s )</controlid>
+        <uniqueid>$Unique</uniqueid>
+        <dtdversion>3.0</dtdversion>
+        <includewhitespace>false</includewhitespace>
+    </control>
+    <operation $( if ($Transaction) {"transaction='true'"} )>
+        <authentication>
+            $Authentication
+        </authentication>
+        <content>
+            $Function
+        </content>
+    </operation>
+</request>
+"@
+
+    Write-Debug "Body: $Body"
+
+    try {
+
+        $Response = Invoke-WebRequest -Method POST -Uri $Uri -Body $Body -ContentType 'application/xml'
+        [xml]$Response.Content
+    }
+    # catch [Microsoft.PowerShell.Commands.HttpResponseException]
+    # {
+    #   Write-Error "$($_.Exception.Response.ReasonPhrase) [$($_.Exception.Response.StatusCode.value__)]"
+    #     # Write-Error "HttpResponseException: $($_.Exception.Response.ReasonPhrase)" # Internal Server Error
+    #     # Write-Error $_.Exception.Message # Response status code does not indicate success: 500 (Internal Server Error).
+    # }
+    catch {
+        Write-Error $_
+    }
+
+}
