8d01cb35681fabd38111dfff6124bae5cd7f35f5
Craig Buchanan <craig.buchanan@cogniza.com>
2020-02-21 14:39:07 -0600

    initial implementation

--- Public/Get-ArPayment.Tests.ps1
+++ Public/Get-ArPayment.Tests.ps1
@@ -1,9 +1,131 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# dot-source dependencies
+$Parent = Split-Path -Parent $here
+. "$Parent/Private/ConvertTo-PlainText.ps1"
+. "$Parent/Private/Send-Request.ps1"
+
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "Get-ArPayment" {
-    It "does something useful" {
-        $true | Should -Be $false
+
+    # arrange
+    $Credential = New-MockObject -Type PsCredential
+    $Session = [PsCustomObject]@{Credential=$Credential;sessionid='abcdefghi';endpoint='https://xxx.yyy.zzz'}
+
+    Context "A/R Payment exists" {
+
+        $Id = 1
+
+        Mock Send-Request {
+
+            $Content = "<?xml version='1.0' encoding='UTF-8'?>
+            <response>
+                <control><status>success</status></control>
+                <operation>
+                    <result>
+                        <status>success</status>
+                        <data listtype='ARPYMT' count='1'>
+                            <ARPYMT>
+                                <RECORDNO>$Id</RECORDNO>
+                            </ARPYMT>
+                        </data>
+                    </result>
+                </operation>
+            </response>"
+            # Write-Debug $Content
+            [xml]$Content
+
+        }
+
+        Context "-Id" {
+
+            # act
+            $Actual = Get-ArPayment -Session $Session -Id $Id
+
+            it "configures the function element properly" {
+                
+                # <function controlid='$Guid'><read><object>ARPYMT</object><keys>1</keys><fields>*</fields></read></function>
+
+                # assert
+                Assert-MockCalled Send-Request -ParameterFilter {
+                    $verb = ([xml]$Function).function.read
+                    $verb.object -eq 'ARPYMT' -and
+                    $verb.keys -eq $Id -and
+                    $verb.fields -eq '*'
+                }    
+            }
+
+            it "returns the correct RECORDNO" {
+                # assert
+                $Actual.RECORDNO | Should -Be $Id
+            }
+
+        }
+    } # /context
+
+    Context "A/R payment not exists" {
+
+        # arrange
+        Mock Send-Request {
+
+            $Content = "<?xml version='1.0' encoding='UTF-8'?>
+            <response>
+                <control><status>success</status></control>
+                <operation>
+                    <result>
+                        <status>success</status>
+                        <data listtype='ARPYMT' count='0'/>
+                    </result>
+                </operation>
+            </response>"
+            # Write-Debug $Content
+            [xml]$Content
+    
+        }
+
+        Context "-Id" {
+            it "writes an non-terminating error that indicates that the A/R payment was not found, including the id" {
+                # act / assert
+                {Get-ArPayment -Session $Session -Id 1000 -ErrorAction Stop } | Should -Throw "A/R Payment 1000 not found"
+            }
+        }
+
     }
+
+    Context "Error encountered" {
+
+        # arrange
+        Mock Send-Request {
+
+            $Content = "<?xml version='1.0' encoding='UTF-8'?>
+            <response>
+                <control><status>success</status></control>
+                <operation>
+                    <result>
+                        <status>failure</status>
+                        <errormessage>
+                            <error>
+                                <errorno>DL02000001</errorno>
+                                <description>Error</description>
+                                <description2>There was an error processing the request.</description2>
+                                <correction>Do something else</correction>
+                            </error>
+                        </errormessage>
+                    </result>
+                </operation>
+            </response>"
+            # Write-Debug $Content
+            [xml]$Content
+    
+        }
+    
+        it "generates a non-terminating error" {
+            # act / assert
+            {Get-ArPayment -Session $Session -Id 0 -ErrorAction Stop } | Should -Throw "There was an error processing the request."
+        }
+
+    } # /context
+
 }
--- Public/Get-ArPayment.ps1
+++ Public/Get-ArPayment.ps1
@@ -1,3 +1,80 @@
 function Get-ArPayment {
 
+    [CmdletBinding()]
+    param
+    (
+		[Parameter(ParameterSetName='ByID', Mandatory)]
+        [pscustomobject]$Session,
+
+        [Parameter(ParameterSetName='ByID', Mandatory)]
+        [int]$Id
+    )
+
+    Write-Debug "$($MyInvocation.MyCommand.Name)"
+
+    $Guid = New-Guid
+
+    $Function = `
+@"
+<function controlid='$Guid'>
+    <read>
+        <object>ARPYMT</object>
+        <keys>$Id</keys>
+        <fields>*</fields>
+    </read>
+</function>
+"@
+
+    try
+    {
+        $Content = Send-Request -Credential $Session.Credential -Session $Session -Function $Function
+
+        Write-Debug "status: $($Content.response.operation.result.status)"
+
+        switch ( $Content.response.operation.result.status )
+        {
+            'success'
+            {  
+                Write-Debug "count: $($Content.response.operation.result.data.count)"
+
+                if ( $Content.response.operation.result.data.count -gt 0 )
+                {
+                    $Content.response.operation.result.data.ARPYMT
+                }
+                else
+                {
+                    Write-Error "A/R Payment $Id not found"
+                }
+
+            }
+            'failure'
+            { 
+                Write-Debug "Module: $($MyInvocation.MyCommand.Module.Name)"
+                Write-Debug "Command: $($MyInvocation.MyCommand.Name)"
+                Write-Debug "description2: $($Content.response.operation.result.errormessage.error.description2)"
+                Write-Debug "correction: $($Content.response.operation.result.errormessage.error.correction)"
+
+                # create ErrorRecord
+                $Exception = New-Object ApplicationException $Content.response.operation.result.errormessage.error.description2
+                $ErrorId = "$($MyInvocation.MyCommand.Module.Name).$($MyInvocation.MyCommand.Name) - $($Content.response.operation.result.errormessage.error.errorno)"
+                $ErrorCategory = [System.Management.Automation.ErrorCategory]::NotSpecified
+                $ErrorRecord = New-Object Management.Automation.ErrorRecord $Exception, $ErrorId, $ErrorCategory, $Content
+
+                # write ErrorRecord
+                Write-Error -ErrorRecord $ErrorRecord -RecommendedAction $Content.response.operation.result.errormessage.error.correction
+            }    
+        } # /switch
+
+    }
+    catch [Microsoft.PowerShell.Commands.HttpResponseException]
+    {
+    # HTTP exceptions
+    Write-Error "$($_.Exception.Response.ReasonPhrase) [$($_.Exception.Response.StatusCode.value__)]"
+    }
+    catch
+    {
+    # all other exceptions
+    Write-Error $_ #.Exception.Message
+    }
+
 }
