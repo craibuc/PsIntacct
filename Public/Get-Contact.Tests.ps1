f5f48d18bb4a466ba9fd5dae711be61f0b4a30b0
Craig Buchanan <craig.buchanan@cogniza.com>
2020-02-21 11:41:24 -0600

    refactoring to delegate work to Send-Request

--- Public/Get-Contact.Tests.ps1
+++ Public/Get-Contact.Tests.ps1
@@ -13,123 +13,259 @@ Describe "Get-Contact" -Tag 'unit' {
     $Credential = New-MockObject -Type PsCredential
     $Session = [PsCustomObject]@{Credential=$Credential;sessionid='abcdefghi';endpoint='https://intacct.com'}
 
-    Context "ByID" {
-
+    Context "Contact exists" {
+    
         # arrange
-        $Id = 123
-        
-        Mock ConvertTo-PlainText {'password'}
-        Mock Invoke-WebRequest
+        $Id = 22
+        $Name = 'Bill Lumbergh'
+    
+        Mock Send-Request {
 
-        it "creates the expected Xml request" {
+            $Content = "<?xml version='1.0' encoding='UTF-8'?>
+            <response>
+                <control><status>success</status></control>
+                <operation>
+                    <result>
+                        <status>success</status>
+                        <data listtype='CONTACT' count='1'>
+                            <CONTACT>
+                                <RECORDNO>$Id</RECORDNO>
+                                <CONTACTNAME>$Name</CONTACTNAME>
+                            </CONTACT>
+                        </data>
+                    </result>
+                </operation>
+            </response>"
+            # Write-Debug $Content
+            [xml]$Content
+
+        }
+
+        Context "-ID" {
+
             # act
-            Get-Contact -Session $Session -Id $Id
+            $Actual = Get-Contact -Session $Session -Id $Id
 
-            # assert
-            Assert-MockCalled ConvertTo-PlainText -Times 1
-            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Method -eq 'Post' }
-            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -eq $Session.endpoint }
-            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $ContentType -eq 'application/xml' }
-            Assert-MockCalled Invoke-WebRequest -ParameterFilter {
-                $xml = ([xml]$Body).request.operation.content.function
-                $xml.ChildNodes[0].ToString() -eq 'read'
-                $xml.read.object.ToString() -eq 'CONTACT'
-                $xml.read.keys.ToString() -eq $Id
-                $xml.read.fields.ToString() -eq '*'
+            it "configures the function element properly" {
+                
+                # <function controlid='$Guid'><read><object>CONTACT</object><keys>$Id</keys><fields>*</fields></read></function>
+
+                # assert
+                Assert-MockCalled Send-Request -ParameterFilter {
+                    $verb = ([xml]$Function).function.read
+                    $verb.object -eq 'CONTACT' -and
+                    $verb.keys -eq $Id -and
+                    $verb.fields -eq '*'
+                }    
             }
-        }
 
-    }
-
-    Context "ByName" {
-
-        # arrange
-        $Name = 'Foo'
-        
-        Mock ConvertTo-PlainText {'password'}
-        Mock Invoke-WebRequest
-
-        it "creates the expected Xml request" {
-            # act
-            Get-Contact -Session $Session -Name $Name
-
-            # assert
-            Assert-MockCalled ConvertTo-PlainText -Times 1
-            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Method -eq 'Post' }
-            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -eq $Session.endpoint }
-            Assert-MockCalled Invoke-WebRequest -ParameterFilter { $ContentType -eq 'application/xml' }
-            Assert-MockCalled Invoke-WebRequest -ParameterFilter {
-                $xml = ([xml]$Body).request.operation.content.function
-                $xml.ChildNodes[0].ToString() -eq 'readByName'
-                $xml.readByName.object.ToString() -eq 'CONTACT'
-                $xml.readByName.keys.ToString() -eq $Name
-                $xml.readByName.fields.ToString() -eq '*'
+            it "returns the correct RECORDNO" {
+                # assert
+                $Actual.RECORDNO | Should -Be $Id
             }
-        }
 
-    }
+            # Mock ConvertTo-PlainText {'password'}
+            # Mock Invoke-WebRequest
 
-    Context "Success" {
+            # it "creates the expected Xml request" {
+            #     # act
+            #     Get-Contact -Session $Session -Id $Id
 
-        # arrange
-        $Name = 'Foo'
-
-        Mock Invoke-WebRequest {
-
-            [PSCustomObject]@{
-                Content = 
-                '<?xml version="1.0" encoding="UTF-8"?>
-                <response>
-                    <control><status>success</status></control>
-                    <operation>
-                        <result>
-                            <status>success</status>
-                            <data listtype="CONTACT" count="1">
-                                <contact><RECORDNO>22</RECORDNO><CONTACTNAME>Bill Lumbergh</CONTACTNAME></contact>
-                            </data>
-                        </result>
-                    </operation>
-                </response>'
-            }
+            #     # assert
+            #     Assert-MockCalled ConvertTo-PlainText -Times 1
+            #     Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Method -eq 'Post' }
+            #     Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -eq $Session.endpoint }
+            #     Assert-MockCalled Invoke-WebRequest -ParameterFilter { $ContentType -eq 'application/xml' }
+            #     Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+            #         $xml = ([xml]$Body).request.operation.content.function
+            #         $xml.ChildNodes[0].ToString() -eq 'read'
+            #         $xml.read.object.ToString() -eq 'CONTACT'
+            #         $xml.read.keys.ToString() -eq $Id
+            #         $xml.read.fields.ToString() -eq '*'
+            #     }
+            # }
 
         }
 
-        it "returns the expected data" {
+        Context "-Name" {
+            
             # act
             $Actual = Get-Contact -Session $Session -Name $Name
 
-            # assert
-            $Actual.Name | Should -Be 'contact'
-            $Actual.RECORDNO | Should -Not -BeNullOrEmpty
-            $Actual.CONTACTNAME | Should -Not -BeNullOrEmpty
+            it "configures the function element properly" {
 
-        }
-    }
+                # <function controlid='$Guid'><readByName><object>CONTACT</object><keys>$Name</keys><fields>*</fields></readByName></function>
 
-    Context "Failure" {
-
-        # arrange
-        $Name = 'Foo'
-        $Description2='lorem ipsum'
-
-        Mock Invoke-WebRequest {
-
-            [PSCustomObject]@{
-                Content = 
-                "<?xml version='1.0' encoding='UTF-8'?>
-                <response>
-                    <control><status>failure</status></control>
-                    <errormessage>
-                        <error><description2>$Description2</description2></error>
-                    </errormessage>
-                </response>"
+                # assert
+                Assert-MockCalled Send-Request -ParameterFilter {
+                    $verb = ([xml]$Function).function.readByName
+                    $verb.object -eq 'CONTACT' -and
+                    $verb.keys -eq $Name -and
+                    $verb.fields -eq '*'
+                }    
             }
 
+            it "returns the correct CONTACTNAME" {
+                # assert
+                $Actual.CONTACTNAME | Should -Be $Name
+            }
+
+            # Mock ConvertTo-PlainText {'password'}
+            # Mock Invoke-WebRequest
+
+            # it "creates the expected Xml request" {
+            #     # act
+            #     Get-Contact -Session $Session -Name $Name
+
+            #     # assert
+            #     Assert-MockCalled ConvertTo-PlainText -Times 1
+            #     Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Method -eq 'Post' }
+            #     Assert-MockCalled Invoke-WebRequest -ParameterFilter { $Uri -eq $Session.endpoint }
+            #     Assert-MockCalled Invoke-WebRequest -ParameterFilter { $ContentType -eq 'application/xml' }
+            #     Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+            #         $xml = ([xml]$Body).request.operation.content.function
+            #         $xml.ChildNodes[0].ToString() -eq 'readByName'
+            #         $xml.readByName.object.ToString() -eq 'CONTACT'
+            #         $xml.readByName.keys.ToString() -eq $Name
+            #         $xml.readByName.fields.ToString() -eq '*'
+            #     }
+            # }
+
         }
 
-        it "throws an exception with the expected text" {
+    }
+
+    Context "Contact not exists" {
+
+        # arrange
+        Mock Send-Request {
+
+            $Content = "<?xml version='1.0' encoding='UTF-8'?>
+            <response>
+                <control><status>success</status></control>
+                <operation>
+                    <result>
+                        <status>success</status>
+                        <data listtype='CONTACT' count='0'/>
+                    </result>
+                </operation>
+            </response>"
+            # Write-Debug $Content
+            [xml]$Content
+    
+        }
+
+        Context "-ID" {
+            it "writes an non-terminating error that indicates that the contact was not found, including the id" {
+                # act / assert
+                {Get-Contact -Session $Session -Id 1000 -ErrorAction Stop } | Should -Throw "Contact 1000 not found"
+            }
+        }
+
+        Context "-Name" {
+            it "writes an non-terminating error that indicates that the contact was not found, including the name" {
             # act / assert
-            {Get-Contact -Session $Session -Name $Name -ErrorAction Stop } | Should -Throw $Description2
+            {Get-Contact -Session $Session -Name 'dummy' -ErrorAction Stop } | Should -Throw "Contact 'dummy' not found"
+            }
+        }
+
+    }
+
+    # Context "Success" {
+
+    #     # arrange
+    #     $Name = 'Foo'
+
+    #     Mock Invoke-WebRequest {
+
+    #         [PSCustomObject]@{
+    #             Content = 
+    #             '<?xml version="1.0" encoding="UTF-8"?>
+    #             <response>
+    #                 <control><status>success</status></control>
+    #                 <operation>
+    #                     <result>
+    #                         <status>success</status>
+    #                         <data listtype="CONTACT" count="1">
+    #                             <contact><RECORDNO>22</RECORDNO><CONTACTNAME>Bill Lumbergh</CONTACTNAME></contact>
+    #                         </data>
+    #                     </result>
+    #                 </operation>
+    #             </response>'
+    #         }
+
+    #     }
+
+    #     it "returns the expected data" {
+    #         # act
+    #         $Actual = Get-Contact -Session $Session -Name $Name
+
+    #         # assert
+    #         $Actual.Name | Should -Be 'contact'
+    #         $Actual.RECORDNO | Should -Not -BeNullOrEmpty
+    #         $Actual.CONTACTNAME | Should -Not -BeNullOrEmpty
+
+    #     }
+    # }
+
+    # Context "Failure" {
+
+    #     # arrange
+    #     $Name = 'Foo'
+    #     $Description2='lorem ipsum'
+
+    #     Mock Invoke-WebRequest {
+
+    #         [PSCustomObject]@{
+    #             Content = 
+    #             "<?xml version='1.0' encoding='UTF-8'?>
+    #             <response>
+    #                 <control><status>failure</status></control>
+    #                 <errormessage>
+    #                     <error><description2>$Description2</description2></error>
+    #                 </errormessage>
+    #             </response>"
+    #         }
+
+    #     }
+
+    #     it "throws an exception with the expected text" {
+    #         # act / assert
+    #         {Get-Contact -Session $Session -Name $Name -ErrorAction Stop } | Should -Throw $Description2
+    #     }
+    # }
+
+    Context "Error encountered" {
+
+        # arrange
+        Mock Send-Request {
+
+            $Content = "<?xml version='1.0' encoding='UTF-8'?>
+            <response>
+                <control><status>success</status></control>
+                <operation>
+                    <result>
+                        <status>failure</status>
+                        <errormessage>
+                            <error>
+                                <errorno>DL02000001</errorno>
+                                <description>Error</description>
+                                <description2>There was an error processing the request.</description2>
+                                <correction>Do something else</correction>
+                            </error>
+                        </errormessage>
+                    </result>
+                </operation>
+            </response>"
+            # Write-Debug $Content
+            [xml]$Content
+    
+        }
+    
+        it "generates a non-terminating error" {
+            # act / assert
+            {Get-Contact -Session $Session -Name 'dummy' -ErrorAction Stop } | Should -Throw "There was an error processing the request."
         }
     }
+
 }
--- Public/Get-Contact.ps1
+++ Public/Get-Contact.ps1
@@ -16,36 +16,41 @@ function Get-Contact {
 
     Write-Debug "$($MyInvocation.MyCommand.Name)"
 
-    Write-Debug "Sender: $($Session.Credential.UserName)"
-    Write-Debug "sessionid: $($Session.sessionid)"
-    Write-Debug "endpoint: $($Session.endpoint)"
+    # Write-Debug "Sender: $($Session.Credential.UserName)"
+    # Write-Debug "sessionid: $($Session.sessionid)"
+    # Write-Debug "endpoint: $($Session.endpoint)"
 
-    $Password = $Session.Credential | ConvertTo-PlainText
-    $Timestamp = Get-Date -UFormat %s
+    # $Password = $Session.Credential | ConvertTo-PlainText
+    # $Timestamp = Get-Date -UFormat %s
     $Guid = New-Guid
 
     $Function = `
     if ($Id)
     {
 @"
-<read>
-    <object>CONTACT</object>
-    <keys>$Id</keys>
-    <fields>*</fields>
-</read>
+<function controlid='$Guid'>
+    <read>
+        <object>CONTACT</object>
+        <keys>$Id</keys>
+        <fields>*</fields>
+    </read>
+</function>
 "@        
     }
     elseif ($Name)
     {
 @"
-<readByName>
-    <object>CONTACT</object>
-    <keys>$Name</keys>
-    <fields>*</fields>
-</readByName>
+<function controlid='$Guid'>
+    <readByName>
+        <object>CONTACT</object>
+        <keys>$Name</keys>
+        <fields>*</fields>
+    </readByName>
+</function>
 "@      
     }
 
+<#
     $Body = `
 @"
 <?xml version="1.0" encoding="UTF-8"?>
@@ -72,28 +77,51 @@ function Get-Contact {
 "@
 
     # Write-Debug $Body
+#>
 
     try
     {
-      $Response = Invoke-WebRequest -Method POST -Uri $Session.endpoint -Body $Body -ContentType 'application/xml'
-
-      $Content = [xml]$Response.Content
+        $Content = Send-Request -Credential $Session.Credential -Session $Session -Function $Function
+        # $Response = Invoke-WebRequest -Method POST -Uri $Session.endpoint -Body $Body -ContentType 'application/xml'
+        # $Content = [xml]$Response.Content
   
-      Write-Debug "status: $($Content.response.control.status)"
+        Write-Debug "status: $($Content.response.operation.result.status)"
 
-      switch ( $Content.response.control.status )
-      {
-        'success'
-        {  
-            $Content.response.operation.result.data.contact
-        }
-        'failure'
-        { 
-          Write-Debug "Error: $($Content.response.errormessage.error.description2)"
-          # raise an exception
-          Write-Error -Message $Content.response.errormessage.error.description2
-        }    
-      } # /switch
+        switch ( $Content.response.operation.result.status )
+        {
+            'success'
+            {  
+                Write-Debug "count: $($Content.response.operation.result.data.count)"
+
+                if ( $Content.response.operation.result.data.count -gt 0 )
+                {
+                    $Content.response.operation.result.data.CONTACT
+                }
+                else
+                {
+                    Write-Error "Contact $( if ($Id) { "$Id" } elseif ($Name) { "'$Name'" } ) not found"
+                }
+
+            }
+            'failure'
+            { 
+                Write-Debug "Module: $($MyInvocation.MyCommand.Module.Name)"
+                Write-Debug "Command: $($MyInvocation.MyCommand.Name)"
+                Write-Debug "description2: $($Content.response.operation.result.errormessage.error.description2)"
+                Write-Debug "correction: $($Content.response.operation.result.errormessage.error.correction)"
+
+                # create ErrorRecord
+                $Exception = New-Object ApplicationException $Content.response.operation.result.errormessage.error.description2
+                $ErrorId = "$($MyInvocation.MyCommand.Module.Name).$($MyInvocation.MyCommand.Name) - $($Content.response.operation.result.errormessage.error.errorno)"
+                $ErrorCategory = [System.Management.Automation.ErrorCategory]::NotSpecified
+                $ErrorRecord = New-Object Management.Automation.ErrorRecord $Exception, $ErrorId, $ErrorCategory, $Content
+
+                # write ErrorRecord
+                Write-Error -ErrorRecord $ErrorRecord -RecommendedAction $Content.response.operation.result.errormessage.error.correction
+                # Write-Error -Message $Content.response.operation.result.errormessage.error.description2 -RecommendedAction $Content.response.operation.result.errormessage.error.correction
+
+            }    
+        } # /switch
 
     }
     catch [Microsoft.PowerShell.Commands.HttpResponseException]
@@ -104,7 +132,7 @@ function Get-Contact {
     catch
     {
       # all other exceptions
-      Write-Error $_.Exception.Message
+      Write-Error $_ #.Exception.Message
     }
 
 }
