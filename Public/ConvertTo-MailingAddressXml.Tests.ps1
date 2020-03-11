7edd77763fdcc319658f7212ae8c2a5e8c5802a6
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-11 14:36:16 -0500

    initial implementation

--- Public/ConvertTo-MailingAddressXml.Tests.ps1
+++ Public/ConvertTo-MailingAddressXml.Tests.ps1
@@ -3,7 +3,40 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "ConvertTo-MailingAddressXml" {
-    It "does something useful" {
-        $true | Should -Be $false
+
+    $Address = [pscustomobject]@{
+        ADDRESS1='Address One'
+        ADDRESS2='Address Two'
+        CITY='Minneapolis'
+        STATE='MN'
+        ZIP='12345'
+        COUNTRYCODE='US'
     }
+
+    Context "Optional fields" {
+
+        it "has 5 parameters" {
+            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter ADDRESS1
+            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter ADDRESS2
+            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter CITY
+            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter STATE
+            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter ZIP
+            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter COUNTRYCODE
+        }
+    
+        it "returns the expected values" {
+            # act
+            [xml]$Actual = $Address | ConvertTo-MailingAddressXml
+
+            # assert
+            $Actual.MAILADDRESS.ADDRESS1 | Should -Be $Address.ADDRESS1
+            $Actual.MAILADDRESS.ADDRESS2 | Should -Be $Address.ADDRESS2
+            $Actual.MAILADDRESS.CITY | Should -Be $Address.CITY
+            $Actual.MAILADDRESS.STATE | Should -Be $Address.STATE
+            $Actual.MAILADDRESS.ZIP | Should -Be $Address.ZIP
+            $Actual.MAILADDRESS.COUNTRYCODE | Should -Be $Address.COUNTRYCODE
+        }
+
+    } # /context
+
 }
