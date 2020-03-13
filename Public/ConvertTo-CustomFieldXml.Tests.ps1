d9210e0853c49b36ca140239cd6a0507b8ecf3dd
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-13 16:58:55 -0400

    adding tag

--- Public/ConvertTo-CustomFieldXml.Tests.ps1
+++ Public/ConvertTo-CustomFieldXml.Tests.ps1
@@ -2,7 +2,7 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "ConvertTo-CustomFieldXml" {
+Describe "ConvertTo-CustomFieldXml" -Tag 'unit' {
 
     $CustomField = [pscustomobject]@{customfieldname='ABC';customfieldvalue=123},[pscustomobject]@{customfieldname='DEF';customfieldvalue='678'}
 
