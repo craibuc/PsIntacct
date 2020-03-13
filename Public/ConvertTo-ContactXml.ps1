d6da29cdf89d7d6d76934805af758eea51bc4e28
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-13 16:55:17 -0400

    formatting

--- Public/ConvertTo-ContactXml.ps1
+++ Public/ConvertTo-ContactXml.ps1
@@ -1,23 +1,64 @@
 <#
-PRINTAS	Required	string	Print as
-CONTACTNAME	Optional	string	If left blank, system will create the name as [NAME](C[CUSTOMERID])
-COMPANYNAME	Optional	string	Company name
-TAXABLE	Optional	boolean	Taxable. Use false for No, true for Yes. (Default: true)
-TAXGROUP	Optional	string	Contact tax group name
-PREFIX	Optional	string	Prefix
-FIRSTNAME	Optional	string	First name
-LASTNAME	Optional	string	Last name
-INITIAL	Optional	string	Middle name
-PHONE1	Optional	string	Primary phone number
-PHONE2	Optional	string	Secondary phone number
-CELLPHONE	Optional	string	Cellular phone number
-PAGER	Optional	string	Pager number
-FAX	Optional	string	Fax number
-EMAIL1	Optional	string	Primary email address
-EMAIL2	Optional	string	Secondary email address
-URL1	Optional	string	Primary URL
-URL2	Optional	string	Secondary URL
-MAILADDRESS	Optional	object	Mail address
+.SYNOPSIS
+Convert Contact model to Xml.
+
+.PARAMETER PRINTAS
+Print as
+
+.PARAMETER CONTACTNAME
+If left blank, system will create the name as [NAME](C[CUSTOMERID])
+
+.PARAMETER COMPANYNAME
+Company name
+
+.PARAMETER TAXABLE
+Taxable. Use false for No, true for Yes. (Default: true)
+
+.PARAMETER TAXGROUP
+Contact tax group name
+
+.PARAMETER PREFIX
+Prefix
+
+.PARAMETER FIRSTNAME
+First name
+
+.PARAMETER LASTNAME
+Last name
+
+.PARAMETER INITIAL
+Middle name
+
+.PARAMETER PHONE1
+Primary phone number
+
+.PARAMETER PHONE2
+Secondary phone number
+
+.PARAMETER CELLPHONE
+Cellular phone number
+
+.PARAMETER PAGER
+Pager number
+
+.PARAMETER FAX
+Fax number
+
+.PARAMETER EMAIL1
+Primary email address
+
+.PARAMETER EMAIL2
+Secondary email address
+
+.PARAMETER URL1
+Primary URL
+
+.PARAMETER URL2
+Secondary URL
+
+.PARAMETER MAILADDRESS
+Mail address
+
 #>
 function ConvertTo-ContactXml {
 
@@ -27,7 +68,7 @@ function ConvertTo-ContactXml {
         [Parameter(ValueFromPipelineByPropertyName,Mandatory)]
         [string]$PRINTAS,
 
-        [Parameter(ValueFromPipelineByPropertyName)]
+        [Parameter(ValueFromPipelineByPropertyName,Mandatory)]
         [string]$CONTACTNAME,
 
         [Parameter(ValueFromPipelineByPropertyName)]
@@ -80,7 +121,6 @@ function ConvertTo-ContactXml {
 
         [Parameter(ValueFromPipelineByPropertyName)]
         [string]$MAILADDRESS
-
     )
 
     Begin
@@ -90,8 +130,11 @@ function ConvertTo-ContactXml {
     }
     Process
     {
+        # mandatory
         if ($PRINTAS) { [void]$SB.Append("<PRINTAS>$PRINTAS</PRINTAS>") }
         if ($CONTACTNAME) { [void]$SB.Append("<CONTACTNAME>$CONTACTNAME</CONTACTNAME>") }
+        # /mandatory
+
         if ($COMPANYNAME) { [void]$SB.Append("<COMPANYNAME>$COMPANYNAME</COMPANYNAME>") }
         [void]$SB.Append("<TAXABLE>$( $TAXABLE.ToString().ToLower() )</TAXABLE>")
         if ($TAXGROUP) { [void]$SB.Append("<TAXGROUP>$TAXGROUP</TAXGROUP>") }
