d280fc6d0976c743f19342923cfb468c0c271bae
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-13 17:01:39 -0400

    adding tag

--- Public/ConvertTo-MailingAddressXml.Tests.ps1
+++ Public/ConvertTo-MailingAddressXml.Tests.ps1
@@ -2,7 +2,7 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "ConvertTo-MailingAddressXml" {
+Describe "ConvertTo-MailingAddressXml" -Tag 'unit' {
 
     $Address = [pscustomobject]@{
         ADDRESS1='Address One'
