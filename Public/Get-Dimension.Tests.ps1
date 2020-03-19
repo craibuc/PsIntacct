484ce046f2df1440f2d6ffe16d08ccd1ae2331fa
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-19 10:47:26 -0500

    initial implementation

--- Public/Get-Dimension.Tests.ps1
+++ Public/Get-Dimension.Tests.ps1
@@ -3,7 +3,67 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "Get-Dimension" {
-    It "does something useful" {
-        $true | Should -Be $false
+
+    # arrange
+    $Credential = New-MockObject -Type PsCredential
+    $Session = [PsCustomObject]@{Credential=$Credential;sessionid='abcdefghi';endpoint='https://xx.yy.zz'}
+
+    Context "Default" {
+
+        BeforeEach {
+            # arrange
+            Mock Send-Request {
+
+                $Content = 
+                "<?xml version='1.0' encoding='UTF-8'?>
+                <response>
+                    <control><status>success</status></control>
+                    <operation>
+                        <result>
+                            <status>success</status>
+                            <data listtype='arinvoice' count='1'>
+                                <dimensions>
+                                    <dimension>
+                                        <objectName>DEPARTMENT</objectName>
+                                        <objectLabel>Department</objectLabel>
+                                        <termLabel>Department</termLabel>
+                                        <userDefinedDimension>false</userDefinedDimension>
+                                        <enabledInGL>true</enabledInGL>
+                                    </dimension>
+                                </dimensions>
+                            </data>
+                        </result>
+                    </operation>
+                </response>"
+                # Write-Debug $Content
+                [xml]$Content
+
+            }
+
+            # act
+            $Actual = Get-Dimension -Session $Session
+        }
+
+        it "configures the function element properly" {
+            
+            # <function controlid='$Guid'><getDimensions/></function>
+
+            # assert
+            Assert-MockCalled Send-Request -ParameterFilter {
+                $function = ([xml]$Function).function
+                $function.getDimensions -ne $null
+            }
+        }
+
+        it "returns the correct data elements" {
+            # assert
+            $Actual.objectName | Should -Not -BeNullOrEmpty
+            $Actual.objectLabel | Should -Not -BeNullOrEmpty
+            $Actual.termLabel | Should -Not -BeNullOrEmpty
+            $Actual.userDefinedDimension | Should -Not -BeNullOrEmpty
+            $Actual.enabledInGL | Should -Not -BeNullOrEmpty
+        }
+
     }
+
 }
--- Public/Get-Dimension.ps1
+++ Public/Get-Dimension.ps1
@@ -1,3 +1,55 @@
 function Get-Dimension {
 
+    [CmdletBinding()]
+    param
+    (
+		[Parameter(Mandatory)]
+        [pscustomobject]$Session
+    )
+
+    Write-Debug "$($MyInvocation.MyCommand.Name)"
+
+    $Guid = New-Guid
+
+    $Function =
+        "<function controlid='$Guid'>
+            <getDimensions/>
+        </function>"
+
+        try
+        {
+            $Content = Send-Request -Credential $Session.Credential -Session $Session -Function $Function
+    
+            Write-Debug "status: $($Content.response.operation.result.status)"
+            switch ( $Content.response.operation.result.status )
+            {
+                'success'
+                {  
+                        $Content.response.operation.result.data.dimensions.dimension
+                }
+                'failure'
+                { 
+                    Write-Debug "Module: $($MyInvocation.MyCommand.Module.Name)"
+                    Write-Debug "Command: $($MyInvocation.MyCommand.Name)"
+                    Write-Debug "description2: $($Content.response.operation.result.errormessage.error.description2)"
+                    Write-Debug "correction: $($Content.response.operation.result.errormessage.error.correction)"
+    
+                    # create ErrorRecord
+                    $Exception = New-Object ApplicationException $Content.response.operation.result.errormessage.error.description2
+                    $ErrorId = "$($MyInvocation.MyCommand.Module.Name).$($MyInvocation.MyCommand.Name) - $($Content.response.operation.result.errormessage.error.errorno)"
+                    $ErrorCategory = [System.Management.Automation.ErrorCategory]::NotSpecified
+                    $ErrorRecord = New-Object Management.Automation.ErrorRecord $Exception, $ErrorId, $ErrorCategory, $Content
+    
+                    # write ErrorRecord
+                    Write-Error -ErrorRecord $ErrorRecord -RecommendedAction $Content.response.operation.result.errormessage.error.correction
+                }
+            } # /switch
+    
+        }
+        catch 
+        {
+            # all other exceptions
+            Write-Error $_.Exception.Message
+        }
+
 }
