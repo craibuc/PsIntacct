4c5208d96ba1db0ac08a07532f69a787cceaa559
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-23 14:54:10 -0500

    initial implementation

---
+++ Public/ConvertTo-ARAdjustmentXml.Tests.ps1
@@ -0,0 +1,93 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "ConvertTo-ARAdjustmentXml" {
+
+    $Adjustment = [pscustomobject]@{
+        customerid=123
+        datecreated='03/01/2020'
+        aradjustmentitems=[pscustomobject]@{}
+    }
+
+    Context "Required fields" {
+
+        it "has 3, mandatory parameters" {
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter customerid -Mandatory -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter datecreated -Mandatory -Type datetime
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter aradjustmentitems -Mandatory -Type pscustomobject[]
+        }
+
+        it "returns the expected values" {
+            # act
+            [xml]$Actual = $Adjustment | ConvertTo-ARAdjustmentXml -Verb 'create'
+
+            # assert
+            $Actual.create_aradjustment.customerid | Should -Be $Adjustment.customerid
+            $Actual.create_aradjustment.datecreated.year | Should -Be ([datetime]$Adjustment.datecreated).ToString('yyyy')
+            $Actual.create_aradjustment.datecreated.month | Should -Be ([datetime]$Adjustment.datecreated).ToString('MM')
+            $Actual.create_aradjustment.datecreated.day | Should -Be ([datetime]$Adjustment.datecreated).ToString('dd')
+        }
+
+    }
+
+    Context "Optional fields" {
+
+        it "has 3, mandatory parameters" {
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter dateposted -Type datetime
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter batchkey -Type int
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter adjustmentno -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter action -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter invoiceno -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter description -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter externalid -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter basecurr -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter currency -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter exchratedate -Type datetime
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter exchratetype -Type string
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter exchrate -Type decimal
+            Get-Command "ConvertTo-ARAdjustmentXml" | Should -HaveParameter nogl -Type bool
+        }
+
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'dateposted' -Value '03/01/2020'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'batchkey' -Value 123
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'adjustmentno' -Value '123'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'action' -Value '123'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'invoiceno' -Value '123'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'description' -Value 'lorem ipsum'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'externalid' -Value '123'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'basecurr' -Value 'USD'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'currency' -Value 'USD'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'exchratedate' -Value '03/01/2020'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'exchratetype' -Value '123'
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'exchrate' -Value 1.0
+        $Adjustment | Add-Member -MemberType NoteProperty -Name 'nogl' -Value $false
+
+        it "returns the expected values" {
+            # act
+            [xml]$Actual = $Adjustment | ConvertTo-ARAdjustmentXml -Verb 'create'
+
+            # assert
+            $Actual.create_aradjustment.customerid | Should -Be $Adjustment.customerid
+            $Actual.create_aradjustment.dateposted.year | Should -Be ([datetime]$Adjustment.dateposted).ToString('yyyy')
+            $Actual.create_aradjustment.dateposted.month | Should -Be ([datetime]$Adjustment.dateposted).ToString('MM')
+            $Actual.create_aradjustment.dateposted.day | Should -Be ([datetime]$Adjustment.dateposted).ToString('dd')
+            $Actual.create_aradjustment.batchkey | Should -Be $Adjustment.batchkey
+            $Actual.create_aradjustment.adjustmentno | Should -Be $Adjustment.adjustmentno
+            $Actual.create_aradjustment.action | Should -Be $Adjustment.action
+            $Actual.create_aradjustment.invoiceno | Should -Be $Adjustment.invoiceno
+            $Actual.create_aradjustment.description | Should -Be $Adjustment.description
+            $Actual.create_aradjustment.externalid | Should -Be $Adjustment.externalid
+            $Actual.create_aradjustment.basecurr | Should -Be $Adjustment.basecurr
+            $Actual.create_aradjustment.currency | Should -Be $Adjustment.currency
+            $Actual.create_aradjustment.exchratedate.year | Should -Be ([datetime]$Adjustment.exchratedate).ToString('yyyy')
+            $Actual.create_aradjustment.exchratedate.month | Should -Be ([datetime]$Adjustment.exchratedate).ToString('MM')
+            $Actual.create_aradjustment.exchratedate.day | Should -Be ([datetime]$Adjustment.exchratedate).ToString('dd')
+            $Actual.create_aradjustment.exchratetype | Should -Be $Adjustment.exchratetype
+            $Actual.create_aradjustment.exchrate | Should -Be $Adjustment.exchrate
+            $Actual.create_aradjustment.nogl | Should -Be $Adjustment.nogl.ToString().ToLower()
+        }
+
+    }
+
+}
---
+++ Public/ConvertTo-ARAdjustmentXml.ps1
@@ -0,0 +1,191 @@
+<#
+.PARAMETER customerid
+Required	string	Customer ID
+
+.PARAMETER datecreated
+Required	object	Transaction date
+
+.PARAMETER dateposted
+Optional	object	GL posting date
+
+.PARAMETER batchkey
+Optional	integer	Summary record number
+
+.PARAMETER adjustmentno
+Optional	string	Adjustment number
+
+.PARAMETER action
+Optional	string	Action. Use Draft or Submit. (Default: Submit)
+
+.PARAMETER invoiceno
+Optional	string	Invoice number
+
+.PARAMETER description
+Optional	string	Description
+
+.PARAMETER externalid
+Optional	string	External ID
+
+.PARAMETER basecurr
+Optional	string	Base currency code
+
+.PARAMETER currency
+Optional	string	Transaction currency code
+
+.PARAMETER exchratedate
+Optional	object	Exchange rate date.
+
+.PARAMETER exchratetype
+Optional	string	Exchange rate type. Do not use if exchrate is set. (Leave blank to use Intacct Daily Rate)
+
+.PARAMETER exchrate
+Optional	currency	Exchange rate value. Do not use if exchangeratetype is set.
+
+.PARAMETER nogl
+Optional	boolean	Do not post to GL. Use false for No, true for Yes. (Default: false)
+
+.PARAMETER aradjustmentitems
+Required	lineitem[1...n]	Invoice lines, must have at least 1.
+
+#>
+function ConvertTo-ARAdjustmentXml {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateSet('create','update','delete')]
+        [string]$Verb,
+
+        [Parameter(ValueFromPipelineByPropertyName,Mandatory)]
+        [string]$customerid,
+
+        [Parameter(ValueFromPipelineByPropertyName,Mandatory)]
+        [datetime]$datecreated,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [datetime]$dateposted,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [int]$batchkey,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$adjustmentno,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$action,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$invoiceno,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$description,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$externalid,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$basecurr,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$currency,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [datetime]$exchratedate,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [string]$exchratetype,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [decimal]$exchrate,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [bool]$nogl,
+
+        [Parameter(ValueFromPipelineByPropertyName,Mandatory)]
+        [pscustomobject[]]$aradjustmentitems
+    )
+
+    <#
+    <create_aradjustment>
+        <customerid>1000</customerid>
+        <datecreated>
+            <year>2013</year>
+            <month>5</month>
+            <day>28</day>
+        </datecreated>
+        <adjustmentno></adjustmentno>
+        <invoiceno></invoiceno>
+        <description></description>
+        <aradjustmentitems>
+            <lineitem>
+                <glaccountno>4000</glaccountno>
+                <amount>1234.56</amount>
+                <memo>test line 1</memo>
+                <locationid>ARL-VA-US</locationid>
+                <departmentid>SAL</departmentid>
+                <projectid></projectid>
+                <customerid></customerid>
+                <vendorid></vendorid>
+                <employeeid></employeeid>
+                <itemid></itemid>
+                <classid></classid>
+            </lineitem>
+        </aradjustmentitems>
+    </create_aradjustment>
+    #>
+
+    begin
+    {
+        $SB = New-Object -TypeName System.Text.StringBuilder
+        [void]$SB.Append("<$($Verb)_aradjustment>")
+    }
+    process
+    {
+        if ($customerid) { [void]$SB.Append("<customerid>$customerid</customerid>") }
+        if ($datecreated) 
+        {
+            [void]$SB.Append(
+            "<datecreated>
+                <year>$( $datecreated.ToString('yyyy') )</year>
+                <month>$( $datecreated.ToString('MM') )</month>
+                <day>$( $datecreated.ToString('dd') )</day>
+            </datecreated>")
+        }
+        if ($dateposted) 
+        {
+            [void]$SB.Append(
+            "<dateposted>
+                <year>$( $dateposted.ToString('yyyy') )</year>
+                <month>$( $dateposted.ToString('MM') )</month>
+                <day>$( $dateposted.ToString('dd') )</day>
+            </dateposted>")
+        }
+        if ($batchkey) { [void]$SB.Append("<batchkey>$batchkey</batchkey>") }
+        if ($adjustmentno) { [void]$SB.Append("<adjustmentno>$adjustmentno</adjustmentno>") }
+        if ($action) { [void]$SB.Append("<action>$action</action>") }
+        if ($invoiceno) { [void]$SB.Append("<invoiceno>$invoiceno</invoiceno>") }
+        if ($description) { [void]$SB.Append("<description>$description</description>") }
+        if ($externalid) { [void]$SB.Append("<externalid>$externalid</externalid>") }
+        if ($basecurr) { [void]$SB.Append("<basecurr>$basecurr</basecurr>") }
+        if ($currency) { [void]$SB.Append("<currency>$currency</currency>") }
+        if ($exchratedate) 
+        {
+            [void]$SB.Append(
+            "<exchratedate>
+                <year>$( $exchratedate.ToString('yyyy') )</year>
+                <month>$( $exchratedate.ToString('MM') )</month>
+                <day>$( $exchratedate.ToString('dd') )</day>
+            </exchratedate>")
+        }
+        if ($exchratetype) { [void]$SB.Append("<exchratetype>$exchratetype</exchratetype>") }
+        if ($exchrate) { [void]$SB.Append("<exchrate>$exchrate</exchrate>") }
+        [void]$SB.Append("<nogl>$( $nogl.ToString().ToLower() )</nogl>")
+        if ($aradjustmentitems) { [void]$SB.Append("<aradjustmentitems>$aradjustmentitems</aradjustmentitems>") }
+    }
+    end
+    {
+        [void]$SB.Append("</$($Verb)_aradjustment>")
+        [xml]$SB.ToString()
+    }
+
+}
