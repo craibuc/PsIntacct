ee25b12c0b19ba701c83bc6204daf15f36297783
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-24 16:34:03 -0500

    adding tag

--- Public/Set-Customer.Tests.ps1
+++ Public/Set-Customer.Tests.ps1
@@ -2,8 +2,8 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "Set-Customer" {
-    It "does something useful" {
+Describe "Set-Customer" -Tag 'unit' {
+    It "does something useful" -Skip {
         $true | Should -Be $false
     }
 }
