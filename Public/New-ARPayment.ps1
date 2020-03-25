368f98ac842259af71661b899e4d1166ac59adec
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-25 10:14:28 -0500

    correcting error handling

--- Public/New-ARPayment.ps1
+++ Public/New-ARPayment.ps1
@@ -24,17 +24,23 @@ function New-ARPayment {
         [xml]$ARPaymentXml
     )
 
-    $Function =
-        "<function controlid='$( New-Guid )'>
-            <create>
-                $( $ARPaymentXml.OuterXml )
-            </create>
-        </function>"
-
-    Write-Debug $Function
-
-    try
+    begin 
     {
+        Write-Debug $MyInvocation.MyCommand.Name
+    }
+
+    process 
+    {
+
+        $Function =
+            "<function controlid='$( New-Guid )'>
+                <create>
+                    $( $ARPaymentXml.OuterXml )
+                </create>
+            </function>"
+
+        Write-Debug $Function
+
         $Content = Send-Request -Credential $Session.Credential -Session $Session -Function $Function
 
         Write-Debug "status: $($Content.response.operation.result.status)"
@@ -46,26 +52,18 @@ function New-ARPayment {
             }
             'failure'
             { 
-                Write-Debug "Module: $($MyInvocation.MyCommand.Module.Name)"
-                Write-Debug "Command: $($MyInvocation.MyCommand.Name)"
-                Write-Debug "description2: $($Content.response.operation.result.errormessage.error.description2)"
-                Write-Debug "correction: $($Content.response.operation.result.errormessage.error.correction)"
+                # return the first error
+                $Err = $Content.response.operation.result.errormessage.FirstChild
+                $ErrorId = "{0}::{1} - {2}" -f $MyInvocation.MyCommand.Module.Name, $MyInvocation.MyCommand.Name, $Err.errorno
+                $ErrorMessage = "{0} [{1}]: {2}" -f $ARPaymentXml.ARPYMT.DOCNUMBER, $ARPaymentXml.ARPYMT.CUSTOMERID, $Err.description2 ?? $Err.errorno
+                $Correction = $Err.correction
 
-                # create ErrorRecord
-                $Exception = New-Object ApplicationException $Content.response.operation.result.errormessage.error.description2
-                $ErrorId = "$($MyInvocation.MyCommand.Module.Name).$($MyInvocation.MyCommand.Name) - $($Content.response.operation.result.errormessage.error.errorno)"
-                $ErrorCategory = [System.Management.Automation.ErrorCategory]::NotSpecified
-                $ErrorRecord = New-Object Management.Automation.ErrorRecord $Exception, $ErrorId, $ErrorCategory, $Content
-
-                # write ErrorRecord
-                Write-Error -ErrorRecord $ErrorRecord -RecommendedAction $Content.response.operation.result.errormessage.error.correction
+                Write-Error -Message $ErrorMessage -ErrorId $ErrorId -Category InvalidArgument -RecommendedAction $Correction -TargetObject $Content.response.operation.result
             }
         } # /switch
 
-    }
-    catch
-    {
-        $_
-    }
+    } # /process
+
+    end {}
 
 }
