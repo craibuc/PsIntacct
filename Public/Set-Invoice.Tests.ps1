c5fa91dec071968600ea9ee9e711f542ce76f9ce
Craig Buchanan <craig.buchanan@cogniza.com>
2020-03-25 10:15:07 -0500

    minor

--- Public/Set-Invoice.Tests.ps1
+++ Public/Set-Invoice.Tests.ps1
@@ -3,7 +3,24 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "Set-Invoice" -Tag 'unit' {
-    It "does something useful" -Skip {
-        $true | Should -Be $false
+
+    # arrange
+    $Credential = New-MockObject -Type PsCredential
+    $Session = [PsCustomObject]@{Credential=$Credential;sessionid='abcdefghi';endpoint='https://intacct.com'}
+
+    $Invoice = [pscustomobject]@{
+        CustomerId='CUSTOMER1'
+        DateCreated= '06/30/2015'
+        # InvoiceItems=@()
     }
+    
+    Context "Parameter validation" {
+
+        it "has 2, mandatory parameters" {
+            Get-Command "Set-Invoice" | Should -HaveParameter Session -Mandatory
+            Get-Command "Set-Invoice" | Should -HaveParameter InvoiceXml -Mandatory -Type xml
+        }
+
+    }
+
 }
