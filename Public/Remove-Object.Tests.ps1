34be8b315485af7f4fce60ee75eaa56f4903851f
Craig Buchanan <craig.buchanan@cogniza.com>
2020-04-15 14:44:18 -0500

    Initial implementation

---
+++ Public/Remove-Object.Tests.ps1
@@ -0,0 +1,202 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Remove-Object" {
+
+    Context "Parameter validation" {
+        $Command = Get-Command 'Remove-Object'
+
+        Context "Session" {
+            $ParameterName = 'Session'
+
+            It "is a [pscustomobject]" {
+                $Command | Should -HaveParameter $ParameterName -Type pscustomobject
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context "Object" {
+            $ParameterName = 'Object'
+
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+            It "supports values from the pipeline by name" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipelineByPropertyName | Should -Be $true
+            }
+        }
+
+        Context "Id" {
+            $ParameterName = 'Id'
+
+            It "is a [int]" {
+                $Command | Should -HaveParameter $ParameterName -Type int
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+            It "supports values from the pipeline by name" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipelineByPropertyName | Should -Be $true
+            }
+            It "has an alias of RECORDNO" {
+                $Command.Parameters[$ParameterName].Aliases | Should -Contain 'RECORDNO'
+            }
+        }
+
+        Context "WhatIf" {
+            it "supports ShouldProcess" {
+            }
+        }
+
+    }
+
+    Context "Success" {
+
+        # arrange
+        $Credential = New-MockObject -Type PsCredential
+        $Session = [PsCustomObject]@{Credential=$Credential;sessionid='0123456789';endpoint='https://xx.yy.cc'}
+
+        $CUSTOMER = [pscustomobject]@{
+            OBJECT = 'CUSTOMER'
+            RECORDNO = 123
+        }
+
+        Mock Send-Request {
+            $Content = 
+            "<response>
+                <control/>
+                <operation>
+                    <authentication/>
+                    <result>
+                        <status>success</status>
+                        <function>delete</function>
+                        <controlid>ebd8d1ec-594b-455d-ae9e-53b62665e201</controlid>
+                    </result>
+                </operation>
+            </response>"
+            [xml]$Content
+        } # /mock
+
+        BeforeEach {
+            # act
+            $Actual = $CUSTOMER | Remove-Object -Session $Session
+        }
+
+        it "calls Send-Request with the correct values" {
+            # assert
+            Assert-MockCalled Send-Request -ParameterFilter {
+                $xml = [xml]$Function
+                $xml.function.delete.object -eq $CUSTOMER.Object -and
+                $xml.function.delete.keys -eq $CUSTOMER.RECORDNO
+            }
+        }
+
+        it "returns a status of 'success" {
+            # assert
+            $Actual.status | Should -Be 'success'
+        }
+
+        it "returns a function of 'delete'" {
+            # assert
+            $Actual.function | Should -Be 'delete'
+        }
+        it "returns a controlid" {
+            # assert
+            $Actual.controlid | Should -Not -BeNullOrEmpty
+        }
+
+    } # /Context
+
+    Context "Failure - Invalid Object" {
+
+        # arrange
+        $Credential = New-MockObject -Type PsCredential
+        $Session = [PsCustomObject]@{Credential=$Credential;sessionid='0123456789';endpoint='https://xx.yy.cc'}
+
+        $CUSTOMER = [pscustomobject]@{
+            OBJECT = 'INVALID'
+            RECORDNO = 1234
+        }
+
+        Mock Send-Request {
+            $Content = 
+            "<response>
+            <control/>
+            <operation>
+                <authentication/>
+                <result>
+                    <status>failure</status>
+                    <function>delete</function>
+                    <controlid>6c8272ad-83f0-413c-b3c9-bb724128d53c</controlid>
+                    <errormessage>
+                        <error>
+                            <errorno>WS</errorno>
+                            <description></description>
+                            <description2>Object definition $( $CUSTOMER.OBJECT ) not found</description2>
+                            <correction></correction>
+                        </error>
+                    </errormessage>
+                </result>
+            </operation>
+        </response>"
+            [xml]$Content
+        } # /Mock
+
+        it "generates a non-terminating error" {
+
+            # assert
+            { $CUSTOMER | Remove-Object -Session $Session -ErrorAction Stop } | Should -Throw "Object definition $( $CUSTOMER.OBJECT ) not found"
+        }
+
+    } # /Context
+
+    Context "Failure - Invalid Id" {
+
+        # arrange
+        $Credential = New-MockObject -Type PsCredential
+        $Session = [PsCustomObject]@{Credential=$Credential;sessionid='0123456789';endpoint='https://xx.yy.cc'}
+
+        $CUSTOMER = [pscustomobject]@{
+            OBJECT = 'CUSTOMER'
+            RECORDNO = 99999
+        }
+
+        Mock Send-Request {
+            $Content = 
+            "<response>
+                <control/>
+                <operation>
+                    <authentication/>
+                    <result>
+                        <status>failure</status>
+                        <function>delete</function>
+                        <controlid>e56a44dd-f252-4c25-8df9-6e151146f374</controlid>
+                        <errormessage>
+                            <error>
+                                <errorno>BL01001973</errorno>
+                                <description></description>
+                                <description2>Cannot find customer with key &#039;$( $CUSTOMER.RECORDNO )&#039; to delete.</description2>
+                                <correction></correction>
+                            </error>
+                        </errormessage>
+                    </result>
+                </operation>
+            </response>"
+            [xml]$Content
+        } # /Mock
+
+        it "generates a non-terminating error" {
+
+            # assert
+            { $CUSTOMER | Remove-Object -Session $Session -ErrorAction Stop } | Should -Throw "Cannot find customer with key '$( $CUSTOMER.RECORDNO )' to delete."
+        }
+
+    } # /Context
+
+}
---
+++ Public/Remove-Object.ps1
@@ -0,0 +1,122 @@
+<#
+.SYNOPSIS
+Delete an object from Intacct.  High potential for a career-ending actions!
+
+.PARAMETER Session
+The object created by the New-Session cmdlet.
+
+.PARAMETER Object
+The type of object to be deleted (e.g. CUSTOMER, CONTACT)
+
+.PARAMETER Id
+The Id (RECORDNO) of the object to be deleted.
+
+.EXAMPLE
+Remove-Object -Object 'CUSTOMER' -Id 1234 -WhatIf
+
+Preview the effects of deleting CUSTOMER 1234.
+
+.EXAMPLE
+Remove-Object -Object 'CUSTOMER' -Id 1234
+
+Confirm
+Are you sure you want to perform this action?
+Performing the operation "Delete" on target "CUSTOMER: 1234".
+[Y] Yes [A] Yes to All [N] No [L] No to All [S] Suspend [?] Help (default is "Yes"): Y
+
+Delete CUSTOMER 1234 after being prompted for confirmation.
+
+.EXAMPLE
+Remove-Object -Object 'CUSTOMER' -Id 1234 -Confirm:$false
+
+Delete CUSTOMER 1234 without confirmation prompting.  DANGEROUS!
+
+.EXAMPLE
+Find-Object -Object 'CUSTOMER' | Remove-Object -Confirm:$false
+
+Delete all CUSTOMER objects without confirmation prompting.  SUPER, SUPER, SUPER DANGEROUS!
+
+#>
+function Remove-Object {
+
+    [CmdletBinding(SupportsShouldProcess,ConfirmImpact='High')]
+    param (
+        [Parameter(Mandatory)]
+        [pscustomobject]$Session,
+
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [string]$Object,
+
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+        [Alias('RECORDNO')]
+        [int]$Id
+    )
+
+    begin { 
+        # Write-Debug "$($MyInvocation.MyCommand.Name)::BEGIN" 
+    }
+
+    process
+    {
+        # Write-Debug "$($MyInvocation.MyCommand.Name)::PROCESS"
+
+        $Function =
+            "<function controlid='$( New-Guid )'>
+                <delete>
+                    <object>$Object</object>
+                    <keys>$Id</keys>
+                </delete>
+            </function>"
+
+        if ($pscmdlet.ShouldProcess("$Object`: $Id", "Delete"))
+        {
+            Write-Verbose "Deleting $Object $Id..."
+
+            $Content = Send-Request -Credential $Session.Credential -Session $Session -Function $Function
+
+            Write-Debug "status: $($Content.response.operation.result.status)"
+            switch ( $Content.response.operation.result.status )
+            {
+                'success'
+                {  
+                    $Content.response.operation.result
+                }
+                'failure'
+                { 
+                    # return the first error
+                    $Err = $Content.response.operation.result.errormessage.FirstChild
+    
+                    # create specific exception if possible
+                    switch ($Err.errorno) {
+                        'BL01001973' # Cannot find customer with key 'XXX' to delete.
+                        {
+                            $ErrorCategory = [System.Management.Automation.ErrorCategory]::ObjectNotFound
+                        }
+                        Default {
+                            $ErrorCategory = [System.Management.Automation.ErrorCategory]::NotSpecified
+                        }
+                    }
+    
+                    $ErrorNumber = $Err.errorno
+                    $ErrorMessage = $Err.description2 ?? $Err.errorno
+                    $Exception = New-Object ApplicationException $ErrorMessage
+                    $ErrorId = "{0}\{1} - {2} [{3}]" -f $MyInvocation.MyCommand.Module.Name, $MyInvocation.MyCommand.Name, $ErrorMessage, $ErrorNumber
+                    # $Correction = $Err.correction
+    
+                    # create ErrorRecord
+                    $ErrorRecord = New-Object Management.Automation.ErrorRecord $Exception, $ErrorId, $ErrorCategory, $Content
+    
+                    # Write-Error -Message $ErrorMessage -ErrorId $ErrorId -Category NotSpecified -RecommendedAction $Correction -TargetObject $Content.response.operation.result
+                    $PSCmdlet.WriteError($ErrorRecord)
+                }
+            } # /switch
+    
+        } # /ShouldProcess
+
+    }
+
+    end { 
+        # Write-Debug "$($MyInvocation.MyCommand.Name)::END" 
+    }
+
+}
