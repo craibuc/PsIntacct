c2cffb2854d7e5b56a2eebdd8ab686e5ad9b1236
Craig Buchanan <craig.buchanan@cogniza.com>
2020-04-30 14:49:16 -0500

    Expanding and improving tests

--- Public/ConvertTo-MailingAddressXml.Tests.ps1
+++ Public/ConvertTo-MailingAddressXml.Tests.ps1
@@ -4,24 +4,107 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 
 Describe "ConvertTo-MailingAddressXml" -Tag 'unit' {
 
-    $Address = [pscustomobject]@{
-        ADDRESS1='Address One'
-        ADDRESS2='Address Two'
-        CITY='Minneapolis'
-        STATE='MN'
-        ZIP='12345'
-        COUNTRYCODE='US'
+    Context "Parameter validation" {
+        $Command = Get-Command "ConvertTo-MailingAddressXml"
+
+        Context "ADDRESS1" {
+            $ParameterName='ADDRESS1'
+
+            it "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            it "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+            it "supports ValueFromPipelineByPropertyName" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipelineByPropertyName | Should -Be $true
+            }
+        }
+
+        Context "ADDRESS2" {
+            $ParameterName='ADDRESS2'
+
+            it "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            it "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+            it "supports ValueFromPipelineByPropertyName" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipelineByPropertyName | Should -Be $true
+            }
+        }
+
+        Context "CITY" {
+            $ParameterName='CITY'
+
+            it "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            it "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+            it "supports ValueFromPipelineByPropertyName" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipelineByPropertyName | Should -Be $true
+            }
+        }
+
+        Context "STATE" {
+            $ParameterName='STATE'
+
+            it "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            it "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+            it "supports ValueFromPipelineByPropertyName" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipelineByPropertyName | Should -Be $true
+            }
+        }
+
+        Context "ZIP" {
+            $ParameterName='ZIP'
+
+            it "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            it "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+            it "supports ValueFromPipelineByPropertyName" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipelineByPropertyName | Should -Be $true
+            }
+        }
+
+        Context "COUNTRYCODE" {
+            $ParameterName='COUNTRYCODE'
+
+            it "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            it "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+            it "supports ValueFromPipelineByPropertyName" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipelineByPropertyName | Should -Be $true
+            }
+            it "has a default value of 'US'" {
+                $Command | Should -HaveParameter $ParameterName -DefaultValue 'US'
+            }
+        }
+
     }
 
-    Context "Optional fields" {
-
-        it "has 5 parameters" {
-            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter ADDRESS1 -Type string
-            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter ADDRESS2 -Type string
-            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter CITY -Type string
-            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter STATE -Type string
-            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter ZIP -Type string
-            Get-Command "ConvertTo-MailingAddressXml" | Should -HaveParameter COUNTRYCODE -Type string
+    Context "Usage" {
+    
+        $Address = [pscustomobject]@{
+            ADDRESS1='Address One'
+            ADDRESS2='Address Two'
+            CITY='Minneapolis'
+            STATE='MN'
+            ZIP='12345'
+            COUNTRYCODE='US'
         }
     
         it "returns the expected values" {
@@ -37,20 +120,20 @@ Describe "ConvertTo-MailingAddressXml" -Tag 'unit' {
             $Actual.MAILADDRESS.COUNTRYCODE | Should -Be $Address.COUNTRYCODE
         }
 
-    } # /context
+        Context "Xml escaping" {
 
-    Context "Xml escaping" {
+            # arrange
+            $Address.ADDRESS1= "Ruth & Steve's"
+            $Address.ADDRESS2= "Ruth & Steve's"
+            $Address.CITY="Coeur d'Alene"
+    
+            it "prevents Xml exceptions" {
+                # act
+                {$Address | ConvertTo-MailingAddressXml -ErrorAction Stop } | Should -Not -Throw
+            }
 
-        # arrange
-        $Address.ADDRESS1= "Ruth & Steve's"
-        $Address.ADDRESS2= "Ruth & Steve's"
-        $Address.CITY="Coeur d'Alene"
-
-        it "prevents Xml exceptions" {
-            # act
-            {$Address | ConvertTo-MailingAddressXml -ErrorAction Stop } | Should -Not -Throw
         }
 
-    }
+    } # /context
 
 }
