496698312fff0e8623e265bbb6f4735ae791b0f4
Craig Buchanan <craig.buchanan@cogniza.com>
2020-04-30 14:38:24 -0500

    Initial implementation

--- Private/ConvertTo-DateXml.Tests.ps1
+++ Private/ConvertTo-DateXml.Tests.ps1
@@ -3,7 +3,49 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "ConvertTo-DateXml" {
-    It "does something useful" {
-        $true | Should -Be $false
+
+    Context "Parameter validation" {
+        $Command = Get-Command 'ConvertTo-DateXml'
+
+        Context "InputObject" {
+            $ParameterName = 'InputObject'
+            
+            it "is a [datetime]" {
+                $Command | Should -HaveParameter $ParameterName -Type datetime
+            }
+            it "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+            it "supports ValueFromPipeline" {
+                $Command.Parameters[$ParameterName].Attributes.ValueFromPipeline | Should -Be $true
+            }
+        }
+
+        Context "Element" {
+            $ParameterName = 'Element'
+            
+            it "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            it "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
     }
+
+    Context "Usage" {
+        # arrange
+        $Expected = Get-Date
+        $Element = 'duedate'
+
+        # act
+        $Actual = $Expected | ConvertTo-DateXml -Element $Element
+
+        # assert
+        $Actual."$Element".year | Should -Be $Expected.Year
+        $Actual."$Element".month | Should -Be $Expected.Month
+        $Actual."$Element".day | Should -Be $Expected.Day
+    }
+
 }
--- Private/ConvertTo-DateXml.ps1
+++ Private/ConvertTo-DateXml.ps1
@@ -1,3 +1,30 @@
 function ConvertTo-DateXml {
 
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [datetime]$InputObject,
+
+        [Parameter(Mandatory)]
+        [string]$Element
+    )
+
+    begin 
+    {
+        $SB = New-Object -TypeName System.Text.StringBuilder
+    }
+    process 
+    {
+        [void]$SB.Append(
+            "<$($Element)>
+                <year>$( $InputObject.ToString('yyyy') )</year>
+                <month>$( $InputObject.ToString('MM') )</month>
+                <day>$( $InputObject.ToString('dd') )</day>
+            </$($Element)>")
+    }
+    end 
+    {
+        [xml]$SB.ToString()
+    }
+
 }
