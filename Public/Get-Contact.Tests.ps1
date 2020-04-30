f2d5cfa6b8c2582a0bce73985353444095dd8b85
Craig Buchanan <craig.buchanan@cogniza.com>
2020-04-30 15:04:57 -0500

    Adding reference to Send-Request

--- Public/Get-Contact.Tests.ps1
+++ Public/Get-Contact.Tests.ps1
@@ -3,6 +3,7 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 # dot-source dependencies
 $Parent = Split-Path -Parent $here
 . "$Parent/Private/ConvertTo-PlainText.ps1"
+. "$Parent/Private/Send-Request.ps1"
 
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
