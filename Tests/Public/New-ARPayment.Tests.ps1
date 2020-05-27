d392e47dac631d728cc5bac3348a71481a07ae0f
Craig Buchanan <craig.buchanan@cogniza.com>
2020-05-27 12:06:22 -0500

    Adding tests for Legacy parameter
    Removing legacy code

--- Tests/Public/New-ARPayment.Tests.ps1
+++ Tests/Public/New-ARPayment.Tests.ps1
@@ -27,7 +27,7 @@ Describe "New-ARPayment" -Tag 'unit' {
         Context "Session" {
             $ParameterName='Session'
             it "is a [pscustobject]" {
-                Get-Command "New-ARPayment" | Should -HaveParameter $ParameterName -Type pscustomobject
+                $Command | Should -HaveParameter $ParameterName -Type pscustomobject
             }
             it "is mandatory" {
                 $Command | Should -HaveParameter $ParameterName -Mandatory
@@ -37,31 +37,38 @@ Describe "New-ARPayment" -Tag 'unit' {
         Context "ARPaymentXml" {
             $ParameterName='ARPaymentXml'
             it "is a [xml]" {
-                Get-Command "New-ARPayment" | Should -HaveParameter $ParameterName -Type xml
+                $Command | Should -HaveParameter $ParameterName -Type xml
             }
             it "is mandatory" {
                 $Command | Should -HaveParameter $ParameterName -Mandatory
             }
         }
+
+        Context "ARPaymentXml" {
+            $ParameterName='Legacy'
+            it "is a [xml]" {
+                $Command | Should -HaveParameter $ParameterName -Type switch
+            }
+        }
     } # /context (parameter validation)
 
-    # arrange
-    $Credential = New-MockObject -Type PsCredential
-    $Expected = @{
-        Session = [PsCustomObject]@{Credential=$Credential;sessionid='abcdefghi';endpoint='https://intacct.com'}
-        Payment = [pscustomobject]@{
-            UNDEPOSITEDACCOUNTNO='0123456789'
-            PAYMENTMETHOD='Cash'
-            CUSTOMERID='ABC'
-            RECEIPTDATE='02/20/2020'
-            CURRENCY='USD'
-            ARPYMTDETAILS = @( [pscustomobject]@{RECORDKEY='123'} )
+    Context "Default parameter values" {
+
+        # arrange
+        $Credential = New-MockObject -Type PsCredential
+
+        $Expected = @{
+            Session = [PsCustomObject]@{Credential=$Credential;sessionid='abcdefghi';endpoint='https://intacct.com'}
+            Payment = [pscustomobject]@{
+                UNDEPOSITEDACCOUNTNO='0123456789'
+                PAYMENTMETHOD='Cash'
+                CUSTOMERID='ABC'
+                RECEIPTDATE='02/20/2020'
+                CURRENCY='USD'
+                ARPYMTDETAILS = @( [pscustomobject]@{RECORDKEY='123'} )
+            }
         }
-    }
 
-    Context "Required parameters" {
-
-        # arrange    
         Mock Send-Request
 
         it "calls Send-Request with the expected parameter values" {
@@ -72,6 +79,39 @@ Describe "New-ARPayment" -Tag 'unit' {
             Assert-MockCalled Send-Request -ParameterFilter {
                 $Session -eq $Expected.Session -and
                 ([xml]$Function).function.create -ne $null
+            }
+        }
+
+    } # /context
+
+    Context "-Legacy" {
+
+        # arrange
+        $Credential = New-MockObject -Type PsCredential
+
+        $Expected = @{
+            Session = [PsCustomObject]@{Credential=$Credential;sessionid='abcdefghi';endpoint='https://intacct.com'}
+            Payment = [pscustomobject]@{
+                undepfundsacct='0123456789'
+                paymentmethod='Cash'
+                paymentamount=100.00
+                customerid='ABC'
+                datereceived='02/20/2020'
+                refid='XXXX'
+                arpaymentitem = @( [pscustomobject]@{invoicekey=123; amount=100.00} )
+            }
+        }
+
+        Mock Send-Request
+
+        it "calls Send-Request with the expected parameter values" {
+            # act
+            $Expected.Payment | ConvertTo-ARPaymentLegacyXml | New-ARPayment -Session $Expected.Session -Legacy
+
+            # assert
+            Assert-MockCalled Send-Request -ParameterFilter {
+                $Session -eq $Expected.Session -and
+                ([xml]$Function).function.create_arpayment -ne $null
             }
         }
 
