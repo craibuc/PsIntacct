fdfe6438ee74eb48a22f19ac9a51a795ac503bd9
Craig Buchanan <craig.buchanan@cogniza.com>
2020-06-12 13:55:58 -0500

    Adding Symlink task

--- psakefile.ps1
+++ psakefile.ps1
@@ -2,6 +2,14 @@ FormatTaskName "-------- {0} --------"
 
 Task default -depends Publish
 
+Task Symlink {
+    $Module='PsIntacct'
+    $Here = Get-Location
+    Push-Location ~/.local/share/powershell/Modules
+    ln -s "$Here/$Module" $Module
+    Pop-Location
+}
+  
 Task Publish {
     publish-module -name ./PsIntacct -Repository Lorenz
 }
