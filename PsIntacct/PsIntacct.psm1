b2b5dc64029f1ce0ac6d8aaae50a722d85e3db30
Craig Buchanan <craig.buchanan@cogniza.com>
2020-11-19 10:00:49 -0600

    adding ErrorAction

--- PsIntacct/PsIntacct.psm1
+++ PsIntacct/PsIntacct.psm1
@@ -2,7 +2,7 @@
 # load (dot-source) *.PS1 files, excluding unit-test scripts (*.Tests.*), and disabled scripts (__*)
 #
 
-"$PSScriptRoot\Public\*.ps1", "$PSScriptRoot\Private\*.ps1" | Get-ChildItem | 
+"$PSScriptRoot\Public\*.ps1", "$PSScriptRoot\Private\*.ps1" | Get-ChildItem -ErrorAction SilentlyContinue | 
     Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
         ForEach-Object {
             # dot-source script
